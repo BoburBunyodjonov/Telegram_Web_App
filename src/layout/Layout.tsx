86edc0ed94cb19de2747271297948b9aecfc70f9
Bobur Bunyodjonov <80701998+BoburBunyodjonov@users.noreply.github.com>
2024-08-07 15:26:29 +0500

    feat:

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "../components"
 const Layout = () => {
   return (
     <>
-         <div className="md:max-w-[500px] container overflow-scroll h-[90vh]  mx-auto">
+         <div className="md:max-w-[500px] container overflow-scroll h-[89vh]  mx-auto">
           <Outlet />
           <Navbar/>
          </div>
